3f1cf3550dae83aebd45264a824f44e42f9f7dd0
Lin <564947814@qq.com>
2020-02-18 18:32:27 +0800

    报修管理接口完成

--- routes/fix.js
+++ routes/fix.js
@@ -4,8 +4,8 @@ const models = require('../models')
 const Op = models.Sequelize.Op
 
 // 获取所有报修信息
-router.get('/getAllFix', async function(req, res, next) {
-  const fixList = await models.fix.findAll().then(fixList => {
+router.get('/getAllFix', function(req, res, next) {
+  const fixList = models.fix.findAll().then(fixList => {
     if (fixList != null) {
       res.json({ state: 200, fixList: fixList })
     } else {
@@ -15,7 +15,7 @@ router.get('/getAllFix', async function(req, res, next) {
 })
 
 // 按条件搜索报修信息
-router.post('/searchFix', async function(req, res, next) {
+router.post('/searchFix', function(req, res, next) {
   let fixInfo = req.body.params.fixInfo
   if (!fixInfo.fixStartTime) {
     fixInfo.fixStartTime = ''
@@ -38,7 +38,7 @@ router.post('/searchFix', async function(req, res, next) {
     }
   }
 
-  const fixList = await models.fix
+  const fixList = models.fix
     .findAll({
       where: where
     })
@@ -51,4 +51,59 @@ router.post('/searchFix', async function(req, res, next) {
     })
 })
 
+// 新增报修信息
+router.post('/addFix', function(req, res, next) {
+  console.log(req.body.params)
+  let fixInfo = req.body.params.fixInfo
+  const fix = models.fix.create(fixInfo).then(fix => {
+    if (fix != null) {
+      res.json({ state: 200, message: '添加成功' })
+    } else {
+      res.json({ state: 400 })
+    }
+  })
+})
+
+// 删除报修信息
+router.post('/deleteFix', function(req, res, next) {
+  const fixInfo = req.body.params.fixInfo
+  const fix = models.fix
+    .destroy({
+      where: {
+        id: fixInfo.id
+      }
+    })
+    .then(fix => {
+      if (fix != null) {
+        res.json({ state: 200, message: '删除成功' })
+      } else {
+        res.json({ state: 400 })
+      }
+    })
+})
+
+// 批量删除
+router.post('/deleteFixList', function(req, res, next) {
+  const fixList = req.body.params.fixList
+  return new Promise((resolve, reject) => {
+    if (!fixList) {
+      reject(error)
+    } else if (fixList.length != 0) {
+      fixList.forEach(item => {
+        let fix = models.fix.destroy({
+          where: {
+            id: item.id
+          }
+        })
+      })
+      resolve()
+    }
+  })
+    .then(data => {
+      res.json({ state: 200, message: '删除成功' })
+    })
+    .catch(error => {
+      res.json({ state: 400 })
+    })
+})
 module.exports = router
